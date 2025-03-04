3800372189365b31904006ba02befd4816a68b6e
2297779320 <2297779320@qq.com>
2025-03-04 10:33:10 +0800

    ada

---
+++ ss.h
@@ -0,0 +1,18 @@
+#ifndef __OVERLAYMANAGER_H__
+#define __OVERLAYMANAGER_H__
+
+#ifdef __cplusplus
+extern "C"{
+#endif /* _cplusplus */
+
+#define BANNER_CHANNEL_NUM	1
+#define BANNER_NUM	    10
+#define BANNER_TOTAL	BANNER_CHANNEL_NUM * BANNER_NUM
+
+
+#ifdef __cplusplus
+}
+#endif /* _cplusplus */
+
+#endif /* __OVERLAYMANAGER_H__ */
+
---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "ss.h"
+
+int main() {
+    // 输出 Hello, World!
+    int a = 20;
+    int b = 30;
+    printf("Hello, World! %d+++++sadasdada+++%d++\n", BANNER_TOTAL, a+b);
+
+    return 0;
+}
